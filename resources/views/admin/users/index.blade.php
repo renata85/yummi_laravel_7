1013b2b3ca35b9f389b870152a94c95a9496237b
batkobatko <novakov_sasa@yahoo.com>
2020-05-18 11:12:50 +0200

    log in and management system update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,11 +8,43 @@
                 <div class="card-header">Users</div>
 
                 <div class="card-body">
-                   @foreach($users as $user)
-                    
-                    {{ $user->name }} - {{ $user->email }}
                    
+                   <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Roles</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($users as $user)
+                    
+                    
+    <tr>
+      <th scope="row">{{ $user->id }}</th>
+      <td>{{ $user->name }}</td>
+      <td>{{ $user->email }}</td>
+      <td>{{ implode(',', $user->roles()->get()->pluck('name')->toArray()) }}</td>
+      <td> 
+            @can('edit-users')  
+            <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button" class="btn btn-primary"class="float-left">Edit</button>
+            <a href="">@endcan
+
+            @can('delete-users')  
+                <form action="{{ route('admin.users.destroy', $user) }}" method="POST" class="float-left">
+                    @csrf
+                    {{ method_field('DELETE') }}
+                 <button type="submit" class="btn btn-secondary">Delete</button>
+            @endcan
+            </form>
+      </td>
+    </tr>
                    @endforeach
+  </tbody>
+</table>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,14 +54,18 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
+                                    @can('manage-users')
                                     <a class="dropdown-item" href="{{ route('admin.users.index') }}"> User Management </a>
+                                    @endcan
 
                                     <form id="logout-form" action="{{route('logout') }}" method="POST" style="display: none;">
                                         @csrf
